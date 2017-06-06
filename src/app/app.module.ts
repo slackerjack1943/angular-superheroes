4f3415b548ef805e7aab578ad45aee1b58b605b8
Jack Cooney <jack.cooney@intelliware.ca>
2017-06-05 23:12:59 -0400

    routing WIP

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,32 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms'; // <-- NgModel lives here
 import { HeroDetailComponent } from './hero-detail.component';
-
+import { HeroesComponent }     from './heroes.component';
 import { AppComponent }  from './app.component';
+import { HeroService } from './hero.service'
+import { RouterModule }   from '@angular/router';
+
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HeroDetailComponent ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: 'heroes',
+        component: HeroesComponent
+      }
+    ])
+  ],
+  declarations: [
+    AppComponent,
+    HeroDetailComponent,
+    HeroesComponent
+  ],
+  providers: [HeroService],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
+
+
+
